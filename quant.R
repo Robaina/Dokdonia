cfada671471fc966e8be18e97b6715824207630f
Semidan <semidan.robaina@gmail.com>
2021-03-05 14:03:04 +0000

    fixed

--- quant.R
+++ quant.R
@@ -1,4 +1,19 @@
+# Workflow: http://monashbioinformaticsplatform.github.io/RNAseq-DE-analysis-with-R/RNAseq_DE_analysis_with_R.html
+
+"""
+FASTA file provided by Jose is too large to be used in buildindex.
+An alternative could be to employ a FASTA with the full genome without annotations
+and use the gtf file after mapping to find annotated sequences. This is described
+in the second web page.
+"""
+# Another one: http://www.compbio.dundee.ac.uk/user/pschofield/Projects/teaching_pg/workshops/biocNGS.html
+# Grant all permissions to working directory: sudo chmod -R a+rwx /path/to/folder
+"""
+Rsubread publication: The R package Rsubread is easier, faster, cheaper and better for alignment and quantification of RNA sequencing reads
+"""
+
 library(Rsubread)
+library(BiocParallel)
 rm(list=ls())
 
 # define shared directory for RNAseq data
@@ -7,7 +22,8 @@ RNAseqDATADIR <- "Data/LauraDokdoniaReadsCleaned"
 fastq_files <- dir(RNAseqDATADIR)
 
 # define the reference genome fasta file
-REF_GENOME <- "Data/DokdoniaMED134.fasta"
+REF_GENOME <- "Data/DokdoniaMED134_full.fasta"
+Annotated_GTF <- "Data/DokdoniaMED134.gtf"
 # define the output directory for the Rsubread index
 # (admin note: requires data/ref_data/download_hg19.sh to be run first)
 RSUBREAD_INDEX_PATH <- "Data/ref_data"
