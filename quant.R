df5c9acd0a8e7fca690847763ca1251f30e13295
Semidan <semidan.robaina@gmail.com>
2021-03-06 10:44:17 +0000

    Added count reads, break code into functions

--- quant.R
+++ quant.R
@@ -1,63 +1,76 @@
-# Workflow: http://monashbioinformaticsplatform.github.io/RNAseq-DE-analysis-with-R/RNAseq_DE_analysis_with_R.html
+"
+This script computes reads from cleaned Fastaq files. It follows the workflow
+described in: http://www.compbio.dundee.ac.uk/user/pschofield/Projects/teaching_pg/workshops/biocNGS.html
 
-"
-FASTA file provided by Jose is too large to be used in buildindex.
-An alternative could be to employ a FASTA with the full genome without annotations
-and use the gtf file after mapping to find annotated sequences. This is described
-in the second web page.
-"
-# Another one: http://www.compbio.dundee.ac.uk/user/pschofield/Projects/teaching_pg/workshops/biocNGS.html
-# Grant all permissions to working directory: sudo chmod -R a+rwx /path/to/folder
-"
 Rsubread publication: The R package Rsubread is easier, faster, cheaper and better for alignment and quantification of RNA sequencing reads
+
+A few notes:
+1. Run script through 'ssh: nohup Rscript quant.R &' to let process continue
+   after ending remote session. Terminal output logged in nohup.out.
+2. buildIndex cannot handle very large fils, that's why I'm not using the
+   annotated fasta file in the server
+3. Grant all permissions to workdir: 'sudo chmod -R a+rwx /path/to/folder'
+4. An additional workflow based in Rsubread can be found at: http://monashbioinformaticsplatform.github.io/RNAseq-DE-analysis-with-R/RNAseq_DE_analysis_with_R.html
+5. Alignment can be parallelized with 'BiocParallel' following directions in the
+   first workflow.
+
+Semidán Robaina Estévez, March 2021.
 "
-
 library(Rsubread)
-# library(BiocParallel)
+
 rm(list=ls())
-
-# define shared directory for RNAseq data
 RNAseqDATADIR <- "Data/LauraDokdoniaReadsCleaned"
-# list the fastq files in the raw data directory
 fastq_files <- dir(RNAseqDATADIR)
-
-# define the reference genome fasta file
 REF_GENOME <- "Data/DokdoniaMED134_full.fasta"
 Annotated_GTF <- "Data/DokdoniaMED134.gtf"
-# define the output directory for the Rsubread index
-# (admin note: requires data/ref_data/download_hg19.sh to be run first)
 RSUBREAD_INDEX_PATH <- "Data/ref_data"
-# define the basename for the index
 RSUBREAD_INDEX_BASE <- "MED134"
-
-# build the index
-buildindex(basename=file.path(RSUBREAD_INDEX_PATH, RSUBREAD_INDEX_BASE), reference=REF_GENOME)
-
-# Alignment
-# define the fastq file with forward reads
 forward_pattern <- "_1.fastq.gz"
 reverses_pattern <- "_2.fastq.gz"
-OUTPUT_PATH <- file.path("Data/BAM_files", "")
+BAM_OUTPUT_PATH <- file.path("Data/BAM_files", "")
 
-files <- dir(RNAseqDATADIR)
-conditions <- vector()
-for (file in files) {
-  if (grepl(forward_pattern, file, fixed=TRUE) == TRUE) {
-     conditions <- c(conditions, strsplit(file, forward_pattern)[[1]])
+function getDataIDs() {
+  files <- dir(RNAseqDATADIR)
+  conditions <- vector()
+  for (file in files) {
+    if (grepl(forward_pattern, file, fixed=TRUE) == TRUE) {
+       conditions <- c(conditions, strsplit(file, forward_pattern)[[1]])
+    }
+  }
+  return conditions
+}
+
+function alignSequences(conditions) {
+  # This loop can be parallelized, see above publication
+  for (condition in conditions) {
+    print(paste0("Aligning condition: ", condition))
+    # Define paired-end fasta files
+    inputfilefwd <- file.path(RNAseqDATADIR, paste0(condition, "_1.fastq.gz"))
+    inputfilervs <- file.path(RNAseqDATADIR, paste0(condition, "_2.fastq.gz"))
+
+    # run the align command to map the reads
+    align(index=file.path(RSUBREAD_INDEX_PATH, RSUBREAD_INDEX_BASE),
+     readfile1=inputfilefwd, readfile2=inputfilervs,
+     output_file=file.path(BAM_OUTPUT_PATH, paste0(condition, ".sam")),
+     output_format="SAM")
   }
 }
 
-# This loop can be parallelized, see above publication
-for (condition in conditions) {
+function countReads() {
+  bamFiles <- list.files(BAM_OUTPUT_PATH)
+  curdir <- getwd()
+  setwd(BAM_OUTPUT_PATH)
+  # Counting reads that match overlap exons and grouping exons by gene_id
+  fcLim <- featureCounts(files = bamFiles,
+    GTF.featureType="exon", GTF.attrType="gene_id",
+    annot.ext=Annotated_GTF, isGTFAnnotationFile=TRUE)
+  setwd(curdir)
 
-  print(paste0("Aligning condition: ", condition))
-  # Define paired-end fasta files
-  inputfilefwd <- file.path(RNAseqDATADIR, paste0(condition, "_1.fastq.gz"))
-  inputfilervs <- file.path(RNAseqDATADIR, paste0(condition, "_2.fastq.gz"))
-
-  # run the align command to map the reads
-  align(index=file.path(RSUBREAD_INDEX_PATH, RSUBREAD_INDEX_BASE),
-   readfile1=inputfilefwd, readfile2=inputfilervs,
-   output_file=file.path(OUTPUT_PATH, paste0(condition, ".sam")),
-   output_format="SAM")
+  knitr::kable(fcLim$stat) # Print stats
+  save(fcLim, file="LauraDokdoniaCounts.RData")
 }
+
+# buildindex(basename=file.path(RSUBREAD_INDEX_PATH, RSUBREAD_INDEX_BASE), reference=REF_GENOME)
+# conditions <- getDataIDs()
+# alignSequences(conditions)
+countReads()
