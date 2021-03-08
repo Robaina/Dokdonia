5739f3be13b815119dcd4494cff2ce8ddb6ee953
Semidán <semidan.robaina@gmail.com>
2021-03-08 15:46:55 +0000

    workday

--- quant.R
+++ quant.R
@@ -21,7 +21,7 @@ rm(list = ls())
 RNAseqDATADIR <- "Data/LauraDokdoniaReadsCleaned"
 fastq_files <- dir(RNAseqDATADIR)
 REF_GENOME <- "Data/DokdoniaMED134_full.fasta"
-Annotated_GTF <- "Data/DokdoniaMED134.gtf"
+Annotated_GTF <- "Data/DokdoniaMED134_fake_MED134_01165.gtf"# "Data/DokdoniaMED134.gtf"
 RSUBREAD_INDEX_PATH <- "Data/ref_data"
 RSUBREAD_INDEX_BASE <- "MED134"
 forward_pattern <- "_1.fastq.gz"
@@ -179,4 +179,4 @@ countReads <- function(paired_end = TRUE, ncores = 14) {
 # buildindex(basename=file.path(RSUBREAD_INDEX_PATH, RSUBREAD_INDEX_BASE), reference=REF_GENOME)
 #conditions <- getDataIDs()
 #alignSequencesBioParallel(conditions, ncores = 6) # alignSequences(conditions, ncores = 14)
-countReads(paired_end = FALSE, ncores = 6)
+countReads(paired_end = FALSE, ncores = 8)
