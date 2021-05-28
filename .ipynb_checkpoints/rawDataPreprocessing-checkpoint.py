62a7c4028f9ef95b35531b8a8f754368e1fc2206
Semidán <semidan.robaina@gmail.com>
2021-05-28 14:52:00 +0100

    w

---
+++ .ipynb_checkpoints/rawDataPreprocessing-checkpoint.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# Script to preprocess raw pair-end read data
+
+# Fastp seems faster, developed by same people of AfterQC: https://github.com/OpenGene/fastp
+
+path_to_afterqc = '/home/robaina/miniconda3/envs/afterqc/bin/after.py'
