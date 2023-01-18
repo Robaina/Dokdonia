7554c4aceeb6237443f4f7485ac98a9e780e4a63
Semidán Robaina Estévez <semidan.robaina@gmail.com>
2023-01-18 18:29:12 +0000

    instll

--- setup.R
+++ setup.R
@@ -1,5 +1,6 @@
 if (!require("BiocManager", quietly = TRUE))
     install.packages("BiocManager")
 
-BiocManager::install("Rsubread", update = FALSE, ask = FALSE)
-BiocManager::install("clusterProfiler", update = FALSE, ask = FALSE)
+BiocManager::install("DESeq2", update = FALSE, ask = FALSE, force=TRUE)
+BiocManager::install("Rsubread", update = FALSE, ask = FALSE, force=TRUE)
+BiocManager::install("clusterProfiler", update = FALSE, ask = FALSE, force=TRUE)
--- setup.sh
+++ setup.sh
@@ -1,22 +1,22 @@
-# Download diffexpr source repo
-echo "Downloading diffexpr repo"
-git clone https://github.com/wckdouglas/diffexpr.git
-cd diffexpr
-
 # Create conda environment containing all dependencies
 echo "Creating conda environment"
 conda env create -q -n dokdonia -f environment.yml
 source activate dokdonia
 
-# Install diffexpr
-echo "Installing diffexpr"
-Rscript setup.R
-python setup.py install
-
 # Install R packages
 echo "Installing R packages"
-cd ..
 Rscript setup.R
+
+# Download diffexpr source repo
+echo "Downloading diffexpr repo"
+git clone https://github.com/wckdouglas/diffexpr.git
+
+# Install diffexpr
+echo "Installing diffexpr"
+cd diffexpr
+Rscript setup.R
+python setup.py -q install
+cd ..
 
 # Delete diffexr repo
 echo "Deleting diffepr repo"
